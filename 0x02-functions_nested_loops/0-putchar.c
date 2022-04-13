d13ac92d5ad6c614bee6195c14171987206b6e0d
mishasharotakala <mishasharotakala@gmail.com>
2022-04-13 13:49:15 -0700

    My commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,12 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+int c;
+char cString[10] = "_putchar\n";
+while (c < 10)
+{
+_putchar(cString[c]);
+c++;
+}
 return (0);
 }
